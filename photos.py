96f0e08003d4d93ff567b2edc378335085f4d72a
Paweł Ryndak <pawelryndak@gmail.com>
2019-03-22 22:25:19 +0100

    Convert bytes to datetime in get_photo_date function

--- photos.py
+++ photos.py
@@ -3,11 +3,18 @@ from datetime import datetime
 
 def get_photo_date(file_path: str) -> datetime:
     photo_details = get_photo_details(file_path)
-    return photo_details['Exif'][piexif.ExifIFD.DateTimeOriginal]
+    photo_details_exif = photo_details['Exif']
+    photo_date_time_field = piexif.ExifIFD.DateTimeOriginal
+    photo_date_time = photo_details_exif[photo_date_time_field]
+    photo_date_time_str = photo_date_time.decode('utf-8')
+    return datetime.strptime(photo_date_time_str, '%Y:%m:%d %H:%M:%S')
     
 def get_photo_details(file_path: str) -> dict():
     return piexif.load(file_path)
     
+
+def list_photos_dates_in_folder():
+    pass
 
 if __name__ == '__main__':
     print(get_photo_date(r'C:\Users\DOM\Pictures\165___07\IMG_0676.JPG'))
